58859d681e6f77feb31ef9c7130106e939795511
Sebastian Arce <sebastianarce25@gmail.com>
2024-11-17 14:09:16 +0000

    shuffle the answers and show a quiz completed div after answering all the questions

--- code/12-quiz-app/src/components/Quiz.jsx
+++ code/12-quiz-app/src/components/Quiz.jsx
@@ -1,22 +1,36 @@
 import { useState } from "react";
 
 import QUESTIONS from "../questions.js";
+import quizCompleteImage from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const isQuizComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(answer) {
     setUserAnswers((prevAnswers) => [...prevAnswers, answer]);
   }
+
+  if (isQuizComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImage} alt="Quiz complete" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = QUESTIONS[activeQuestionIndex].answers;
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="questions">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+          {shuffledAnswers.map((answer, index) => (
             <li key={index} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
