7fc60dd50fae5918751d7e5f9bfec957fdf6d521
Sebastian Arce <sebastianarce25@gmail.com>
2024-11-18 08:28:03 +0000

    migrate backend from Express to FastAPI; add CORS support, implement routes for places and user places, and update project structure

---
+++ code/13-http-requests/backend/app.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+import json
+import os
+
+app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["GET", "PUT"],
+    allow_headers=["Content-Type"],
+)
+
+
+# Static files
+@app.get("/images/{file_path:path}")
+async def read_image(file_path: str):
+    file_location = os.path.join("images", file_path)
+    if os.path.exists(file_location):
+        return FileResponse(file_location)
+    else:
+        raise HTTPException(status_code=404, detail="File not found")
+
+
+# Models
+class Place(BaseModel):
+    id: int
+    name: str
+    description: str
+
+
+@app.get("/places")
+async def get_places():
+    with open("./data/places.json", "r") as file:
+        places_data = json.load(file)
+    return {"places": places_data}
+
+
+@app.get("/user-places")
+async def get_user_places():
+    with open("./data/user-places.json", "r") as file:
+        places = json.load(file)
+    return {"places": places}
+
+
+@app.put("/user-places")
+async def update_user_places(place: Place):
+    with open("./data/user-places.json", "r") as file:
+        places = json.load(file)
+
+    places.append(place.model_dump())
+
+    with open("./data/user-places.json", "w") as file:
+        json.dump(places, file, indent=4)
+
+    return {"message": "Place updated successfully"}
